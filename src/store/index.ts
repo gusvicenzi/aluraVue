216575a9fd9be6adbb698625734f9cb31dbb5e6c
Gustavo Vicenzi <gusvicenzi12@gmail.com>
2023-11-13 10:28:06 -0300

    Add adiciona_projeto mutation to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,6 @@
 import { IProjeto } from '@/interfaces/IProjeto'
 import { InjectionKey } from 'vue'
-import { Store, createStore } from 'vuex'
+import { Store, createStore, useStore as useStoreVuex } from 'vuex'
 
 interface Estado {
   projetos: IProjeto[]
@@ -10,15 +10,20 @@ export const key: InjectionKey<Store<Estado>> = Symbol()
 
 export const store = createStore<Estado>({
   state: {
-    projetos: [
-      {
+    projetos: []
+  },
+  mutations: {
+    ADICIONA_PROJETO(state, nomeDoProjeto: string) {
+      const projeto = {
         id: Date.now().toString(),
-        nome: 'TypeScript'
-      },
-      {
-        id: Date.now().toString(),
-        nome: 'Vue'
-      }
-    ]
+        nome: nomeDoProjeto
+      } as IProjeto
+
+      state.projetos.push(projeto)
+    }
   }
 })
+
+export function useStore(): Store<Estado> {
+  return useStoreVuex(key)
+}
