a94a882eaa6c0c2f7aa898ee2377d52b385edd70
Gustavo Vicenzi <gusvicenzi12@gmail.com>
2023-11-13 11:44:39 -0300

    Grouped projetos routes and separate list

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,49 +1,15 @@
 <template>
   <section class="projetos">
     <h1 class="title">Projetos</h1>
-    <RouterLink to="/projetos/novo" class="button">
-      <span class="icon is-small">
-        <i class="fas fa-plus"></i>
-      </span>
-      <span>Novo projeto</span>
-    </RouterLink>
-    <table class="table is-fullwidth">
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Nome</th>
-          <th>Ações</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="projeto in projetos" :key="projeto.id">
-          <td>{{ projeto.id }}</td>
-          <td>{{ projeto.nome }}</td>
-          <td>
-            <RouterLink :to="`/projetos/${projeto.id}`" class="button">
-              <span class="icon is-small">
-                <i class="fas fa-pencil-alt"></i>
-              </span>
-            </RouterLink>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <RouterView />
   </section>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue';
-import { useStore } from '../store/index'
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'ProjetosView',
-  setup() {
-    const store = useStore()
-    return {
-      projetos: computed(() => store.state.projetos)
-    }
-  }
 });
 </script>
 
--- src/views/projetos/Formulario.vue
+++ src/views/projetos/Formulario.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="projetos">
+  <section>
     <h1 class="title">Projetos</h1>
     <form @submit.prevent="salvar">
       <div class="field">
@@ -59,9 +59,3 @@ export default defineComponent({
   }
 });
 </script>
-
-<style scoped>
-.projetos {
-  padding: 1.25rem;
-}
-</style>
